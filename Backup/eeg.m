b09999d35c8142279126e78e7c3c9dad8eb3d507
shahriariit <shahriar.setu@gmail.com>
2018-06-22 02:00:10 +0600

    Update

---
+++ Backup/eeg.m
@@ -0,0 +1,15 @@
+for i=1:10
+    for j=1:15
+       plot(num{i, 1}(:,[2,j]),'DisplayName','num{5, 1}(:,[1,3])'); 
+       saveas(gcf,['plot_',num2str(i),num2str(j)],'jpg');
+    end
+end
+
+for i=11:20
+    for j=1:15
+       plot(num{i, 1}(:,[1,j]),'DisplayName','num{5, 1}(:,[1,3])'); 
+       saveas(gcf,['plot_',num2str(i),num2str(j)],'jpg');
+    end
+end
+
+
